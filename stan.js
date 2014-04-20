2cbd3e9a9dce2491f3e2f823a89e43b99addd31e
Ai_boy <ai_boy@live.ru>
2014-04-21 01:40:19 +0600

    just made Crockford Mad :)

--- stan.js
+++ stan.js
@@ -9,22 +9,22 @@ STAN = {
         	.replace(/\.(div|span)/g,')+"</$1>"')
   		while (ms = result.match(/partial\([_\w\d\.]+/g)) {
      		for (var i=0; i< ms.length; i++) {
-       			var tn = ms[i].replace('partial(','');
-       			result = result.replace(tn, compile(eval(tn), context).replace('})()','})'));
+       			var tn = ms[i].replace('partial(','')
+       			result = result.replace(tn, compile(eval(tn), context).replace('})()','})'))
      		}
   		}
-  		return result;
+  		return result
 	},
 	run: function(result, context, escape) {
-		var r = "", raw = function(t){return t;}
+		var r = "", raw = function(t){return t}
   		if (escape == null || escape == true) { 
     		context = JSON.parse(JSON.stringify(context)
         		.replace(/</g,'&lt;')
-        		.replace(/>/g,'&gt;'))};
+        		.replace(/>/g,'&gt;'))}
   		function partial(fn, ctx) {
-      		var oldCtx = context; 
-      		context = ctx; fn(); context = oldCtx;
+      		var oldCtx = context
+      		context = ctx; fn(); context = oldCtx
  		}
-  		return eval(result), r;
+  		return eval(result), r
 	}
 }
