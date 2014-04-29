2d80005a6359a41a1383e42e14f7e13a659eebce
Oleksii Okhrymenko <12@MacBook-Pro-1.local>
2014-04-29 13:55:16 +0600

    apdeit

--- stan.js
+++ stan.js
@@ -2,10 +2,10 @@ STAN = {
     compile: function(_template_) {
         var ms, result = _template_.toString()
             .replace(/function\s+([\d\w_]+)\s*\(\s*\)\s*{([\S\s]+)}/,'(function $1(){$2})()')
-            .replace(/(div|span)\.b/g, 'r+="<$1>";')
-            .replace(/(div|span)\.e/g, 'r+="</$1>";')
-            .replace(/(div|span)(?:\['([^.']+)'\])?\./g,"r+='<$1 $2>'+(")
-            .replace(/\.(div|span)/g,')+"</$1>"')
+            .replace(/(\w+)\.(e|b)\s*[\n;}]/g, function(m,t,e){ return "r+='<" + (e=="b"?"":"/") + t + ">';" })
+            .replace(/(\w+)\.(.+)\.(\w+)\s*[\n;}]/g, function(m,s,b,e){ 
+                if(s==e) {return "r+='<" + s + ">'+(" + b + ")+'</" + e + ">';"} else return m })
+            .replace(/([\n;{]\s*)(\w+)(\s*[\n;}])/g, "$1r+='<$2/>'$3;")
         while (ms = result.match(/partial\([_\w\d\.]+/g)) {
             for (var i=0; i< ms.length; i++) {
                 var tn = ms[i].replace('partial(','')
@@ -16,10 +16,11 @@ STAN = {
     },
     run: function(result, context, escape) {
         var r = "", raw = function(t){return t}
-        if (escape == null || escape == true) { 
-        context = JSON.parse(JSON.stringify(context)
-            .replace(/</g,'&lt;')
-            .replace(/>/g,'&gt;'))}
+        if (escape !== false) { 
+            var div = document.createElement('div')
+            div.appendChild(document.createTextNode(JSON.stringify(context)))
+            context = JSON.parse(div.innerHTML)
+        }
         function partial(fn, ctx) {
             var oldCtx = context
             context = ctx; fn(); context = oldCtx
