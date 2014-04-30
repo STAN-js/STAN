4c97ca43ac06e245896cf57b288ac5fc581d2adb
Alexey Ohrimenko <oleksii@yandex-team.ru>
2014-04-30 12:51:37 +0600

    examples changed to new style of attribute editing. Now any attributes can be added but only static ones

--- stan.js
+++ stan.js
@@ -2,9 +2,9 @@ STAN = {
     compile: function(_template_) {
         var ms, result = _template_.toString()
             .replace(/function\s+([\d\w_]+)\s*\(\s*\)\s*{([\S\s]+)}/,'(function $1(){$2})()')
-            .replace(/(\w+)\.(e|b)\s*[\n;}]/g, function(m,t,e){ return "r+='<" + (e=="b"?"":"/") + t + ">';" })
-            .replace(/(\w+)\.(.+)\.(\w+)\s*[\n;}]/g, function(m,s,b,e){ 
-                if(s==e) {return "r+='<" + s + ">'+(" + b + ")+'</" + e + ">';"} else return m })
+            .replace(/(\w+)(?:\[['"]([^\]]*)['"]\])?\.(e|b)\s*[\n;}]/g, function(m,t,x,e){ return "r+='<" + (e=="b"?"":"/") + t + " " + x + ">';" })
+            .replace(/(\w+)(?:\[['"]([^\]]*)['"]\])?\.(.+)\.(\w+)\s*[\n;}]/g, function(m,s,x,b,e){
+                if(s==e) {return "r+='<" + s + " " + x + ">'+(" + b + ")+'</" + e + ">';"} else return m })
             .replace(/([\n;{]\s*)(\w+)(\s*[\n;}])/g, "$1r+='<$2/>'$3;")
         while (ms = result.match(/partial\([_\w\d\.]+/g)) {
             for (var i=0; i< ms.length; i++) {
