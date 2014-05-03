a1280d028a131344f39c43579632464810e92acf
Oleksii Okhrymenko <ai_boy@live.ru>
2014-05-04 04:17:54 +0600

    now JSON output ( or any other format ) is possible via 'raw' method

--- stan.js
+++ stan.js
@@ -2,10 +2,10 @@ STAN = {
     compile: function(_template_) {
         var ms, result = _template_.toString()
             .replace(/function\s+([\d\w_]+)\s*\(\s*\)\s*{([\S\s]+)}/,'(function $1(){$2})')
-            .replace(/(\w+)(?:\[['"]([^\]]*)['"]\])?\.(e|b)\s*[\n;}]/g, function(m,t,x,e){ return "r+='<" + (e=="b"?"":"/") + t + " " + (x||'') + ">';" })
+            .replace(/(\w+)(?:\[['"]([^\]]*)['"]\])?\.(e|b)\s*[\n;}]/g, function(m,t,x,e){ return "_r+='<" + (e=="b"?"":"/") + t + " " + (x||'') + ">';" })
             .replace(/(\w+)(?:\[['"]([^\]]*)['"]\])?\.(.+)\.(\w+)\s*[\n;}]/g, function(m,s,x,b,e){
-                if(s==e) {return "r+='<" + s + " " + (x||'') + ">'+(" + b + ")+'</" + e + ">';"} else return m })
-            .replace(/([\n;{]\s*)(\w+)(\s*[\n;}])/g, "$1r+='<$2/>'$3;")
+                if(s==e) {return "_r+='<" + s + " " + (x||'') + ">'+(" + b + ")+'</" + e + ">';"} else return m })
+            .replace(/([\n;{]\s*)(\w+)(\s*[\n;}])/g, "$1_r+='<$2/>'$3;")
         while (ms = result.match(/partial\([_\w\d\.]+/g)) {
             for (var i=0; i< ms.length; i++) {
                 var tn = ms[i].replace('partial(','')
@@ -15,7 +15,7 @@ STAN = {
         return result + '()'
     },
     run: function(result, context, escape) {
-        var r = "", raw = function(t){return t}
+        var _r = "", raw = function(t){ _r+=t; return _r }
         if (escape !== false) { 
             var div = document.createElement('div')
             div.appendChild(document.createTextNode(JSON.stringify(context)))
@@ -25,6 +25,6 @@ STAN = {
             var oldCtx = context
             context = ctx; fn(); context = oldCtx
         }
-        return eval(result), r
+        return eval(result), _r
     }
 }
