921292570b8f238cc73fdfae731254d439e684da
Oleksii Okhrymenko <ai_boy@live.ru>
2014-06-08 00:15:34 +0600

    STAN - support js functions inside of 'attributes' declaration

--- stan.js
+++ stan.js
@@ -2,9 +2,10 @@ STAN = {
     compile: function(_template_) {
         var ms, result = _template_.toString()
             .replace(/function\s+([\d\w_]+)\s*\(\s*\)\s*{([\S\s]+)}/,'(function $1(){$2})()')
-            .replace(/(\w+)(?:\[['"]([^\]]*)['"]\])?\.(e|b)\s*[\n;}]/g, function(m,t,x,e){ return "r+='<" + (e=="b"?"":"/") + t + " " + (x||'') + ">';" })
-            .replace(/(\w+)(?:\[['"]([^\]]*)['"]\])?\.(.+)\.(\w+)\s*[\n;}]/g, function(m,s,x,b,e){
-                if(s==e) {return "r+='<" + s + " " + (x||'') + ">'+(" + b + ")+'</" + e + ">';"} else return m })
+            .replace(/(\w+)(?:\[(['"]?[^\]]*?['"]?)\])?\.(e|b)\s*[\n;}]/g, function(m,t,x,e){ 
+                return "r+='<" + (e=="b"?"":"/") + t + " '+(" + (x||'""') + ")+'>';" })
+            .replace(/(\w+)(?:\[(['"]?[^\]]*?['"]?)\])?\.(.+)\.(\w+)\s*[\n;}]/g, function(m,s,x,b,e){
+                if(s==e) {return "r+='<" + s + " '+(" + (x||'""') + ")+'>'+(" + b + ")+'</" + e + ">';"} else return m })
             .replace(/([\n;{]\s*)(\w+)(\s*[\n;}])/g, "$1r+='<$2/>'$3;")
         while (ms = result.match(/partial\([_\w\d\.]+/g)) {
             for (var i=0; i< ms.length; i++) {
@@ -22,8 +23,7 @@ STAN = {
             context = JSON.parse(div.innerHTML)
         }
         function partial(fn, ctx) {
-            var oldCtx = context
-            context = ctx; fn(); context = oldCtx
+            var oldCtx = context; context = ctx; fn(); context = oldCtx
         }
         return eval(result), r
     }
